f2d8048dfd3e611785f1492de9bd639cdcb84349
Mathias Vatter <mvatter@students.uni-mainz.de>
2024-12-02 19:29:57 +0100

    Update lexer with builtins imported from .txt files

--- pygments_lexers/cksp_lexer.py
+++ pygments_lexers/cksp_lexer.py
@@ -1,5 +1,6 @@
-from pygments.lexer import RegexLexer, bygroups
+from pygments.lexer import RegexLexer, bygroups, include
 from pygments.token import Text, Keyword, Name, String, Number, Operator, Comment, Whitespace, Punctuation
+import os
 
 class CKSPLexer(RegexLexer):
     name = 'CKSP'
@@ -8,6 +9,67 @@ class CKSPLexer(RegexLexer):
 
     var_name = r'[0-9#]*[a-zA-Z_#]+[0-9a-zA-Z_#.]*'
 
+    def load_builtin_functions(filename):
+        filepath = os.path.join(os.path.dirname(__file__), filename)
+        functions = []
+        with open(filepath, 'r') as f:
+            for line in f:
+                # Entferne alles nach "//" (Kommentare)
+                line = line.split('//', 1)[0].strip()
+                
+                # Überspringe leere Zeilen
+                if not line:
+                    continue
+                
+                # Schneide alles nach der ersten Klammer ab
+                function = line.split('(', 1)[0].strip()
+                
+                # Füge die Funktion zur Liste hinzu (falls nicht leer)
+                if function:
+                    functions.append(function)
+        return functions
+    
+    def load_builtin_variables(filename):
+        filepath = os.path.join(os.path.dirname(__file__), filename)
+        vars = []
+        with open(filepath, 'r') as f:
+            for line in f:
+                line = line.split('//', 1)[0].strip()
+                
+                # Überspringe leere Zeilen
+                if not line:
+                    continue
+                
+                types = ['$', '%', '~', '?', '@']
+                var = line.strip()
+                if(var[0] in types):
+                    var = var[1:]
+            
+                vars.append(var)
+        return vars
+    
+    def load_builtin_widgets(filename):
+        filepath = os.path.join(os.path.dirname(__file__), filename)
+        widgets = []
+        with open(filepath, 'r') as f:
+            for line in f:
+                line = line.split('//', 1)[0].strip()
+                
+                # Überspringe leere Zeilen
+                if not line:
+                    continue
+
+                widget = line.split(' ', 1)[0].strip()            
+                widgets.append(widget)
+        return widgets
+    
+    builtin_functions_file = "cksp_builtins/engine_functions.txt"
+    builtin_variables_file = "cksp_builtins/engine_variables.txt"
+    builtin_widgets_file = "cksp_builtins/engine_widgets.txt"
+    builtin_funcs = load_builtin_functions(builtin_functions_file)
+    builtin_vars = load_builtin_variables(builtin_variables_file)
+    builtin_widgets = load_builtin_widgets(builtin_widgets_file)
+
     tokens = {
         'root': [
 
@@ -15,12 +77,6 @@ class CKSPLexer(RegexLexer):
             (r'//.*$', Comment.Single),
             (r'/\*.*?\*/', Comment.Multiline),
             (r'{.*?}', Comment.Multiline),
-
-            # Parentheses
-            (r'\(', Punctuation, '#push'),  # Betritt den aktuellen State erneut
-            (r'\)', Punctuation, '#pop'),  # Verlässt den aktuellen State
-            (r'\[', Punctuation, '#push'),  # Betritt den aktuellen State erneut
-            (r'\]', Punctuation, '#pop'),  # Verlässt den aktuellen State
 
             # Numbers
             (r'\b0x[0-9a-fA-F]+\b', Number.Hex),
@@ -33,57 +89,92 @@ class CKSPLexer(RegexLexer):
 
             # Keywords
             (r'\b(continue|break|return|for|in|to|steps|end\s+for|if|else|end\s+if|'
-             r'select|case|end\s+select|while|end\s+while)\b', Keyword.Control),
+             r'select|case|end\s+select|while|end\s+while)\b', Keyword.Constant),
+
+            # Import
+            (r'\b(import)(\s+)', bygroups(Keyword, Text)),
+
+            # Operators
+            (r'(-|\+|\/|\*|\*\*|>>|<<|=|<|>|<=|>=|\#|->)', Operator),
+            (r'(and|or|not|xor|.and.|.or.|.not.|.xor.)\b', Operator.Word),
 
             # Constants
             (r'\b(true|false|nil)\b', Name.Constant),
 
-            # Built-in Functions
-            (r'\b(abs|acos|sin|cos|tan|pow|sqrt|log|exp|floor|ceil|round)\b', Name.Builtin),
-
-            # Built-in Constants (abgekürzt)
-            (r'\b(message|ALL_EVENTS|CC_NUM|EVENT_PAR|FILTER_TYPE_AR_BP2|'
-             r'LOOP_PAR_START|NI_WAVEFORM_TYPE)\b', Name.Builtin),
-
-            # Operators
-            (r'(-|\+|\/|\*|\*\*|>>|<<|=|<|>|<=|>=|\#)', Operator),
-
             # Preprocessor Directives and Defines
-            (r'\b(START_INC|END_INC|SET_CONDITION|#pragma|define|import)\b', Keyword.Preproc),
+            (r'\b(START_INC|END_INC|SET_CONDITION|RESET_CONDITION|#pragma|import)\b', Keyword.Preproc),
+
+            include('builtins'),
+            include('widgets'),
 
             # Callbacks
             (r'\bon\s+(async_complete|controller|init|listener|note|persistence_changed|'
              r'pgs_changed|poly_at|release|rpn|nrpn|ui_control|ui_update|midi_in)\b', Keyword),
             (r'\bend\s+on\b', Keyword),
 
+            # Function Call
+            (r'\b(call)\s+('+var_name+r')(\()', bygroups(Keyword.Declaration, Name.Function, Text)),
+            (r'\b('+var_name+r')(\()', bygroups(Name.Function, Text)),  # Ohne "call"
+
+            # Defines
+            (r'\b(define)(\s+)', bygroups(Keyword.Declaration, Text), 'macroname'),
+
             # Macros
-            (r'\bmacro\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*\(', Keyword, 'macro'),
-            (r'\bend\s+macro\b', Keyword),
+            (r'\b(macro)(\s+)', bygroups(Keyword.Declaration, Text), 'macroname'),
+            (r'\bend\s+macro\b', Keyword.Declaration),
 
-            # (r'(function)(\s+)',
-            # bygroups(Keyword.Declaration, Text), 'funcname'),
+            # Functions
+            (r'\b(function)(\s+)', bygroups(Keyword.Declaration, Text), 'funcname'),
+            (r'\b(end\s+function)', Keyword.Declaration),
 
-            # Structs and Functions
-            (r'\b(struct|end\s+struct|function|end\s+function)\b', Keyword),
-            # (r'\b([a-zA-Z_][a-zA-Z0-9_]*)\s*\(', Name.Function),
+            # Structs
+            (r'\b(struct)(\s+)', bygroups(Keyword.Declaration, Text), 'structname'),
+            (r'\b(end\s+struct)', Keyword.Declaration),
 
-            # Variable with Type
-            (r'('+var_name+r')(\s*:\s*)(int|float|string)',
-                bygroups(Name.Variable, Text, Keyword.Type)),
+            (r'(self)(?:(.)?)\b', bygroups(Name.Builtin.Pseudo, Text)),
+            
+            # Type
+            (r'\b(:\s*)(int|real|string|bool|void)\b', bygroups(Text, Keyword.Type)),
+            (r'(\$|\!|\%|\~|\?|\@)', Keyword.Type),
+
+            # everything with letters after : is a type
+            (r'(:\s*)('+var_name+r')', bygroups(Text, Keyword.Type)),
 
             # Variable declarations
-            (r'\b(declare|delete|local|global|pers|instpers|const)\b', Keyword.Declaration),
+            (r'\b(declare|delete|local|global|pers|instpers|const|polyphonic)\b', Keyword.Declaration),
 
             # Variable References
             (var_name, Name.Variable),
             (r'[,;:]', Text),
-            (r'[\s+]', Whitespace)
+            (r'[\s+]', Whitespace),
+            # Parentheses
+            (r'\(', Punctuation, '#push'),  # Betritt den aktuellen State erneut
+            (r'\)', Punctuation, '#pop'),  # Verlässt den aktuellen State
+            (r'\[', Punctuation, '#push'),  # Betritt den aktuellen State erneut
+            (r'\]', Punctuation, '#pop'),  # Verlässt den aktuellen State
+
         ],
         
         # Macro-specific tokens
-        'macro': [
-            (r'\)', Text, '#pop'),  # End of macro definition
-            (r'\b[a-zA-Z_][a-zA-Z0-9_]*\b', Name.Variable),  # Macro parameters
-            (r'\s+', Text)  # Allow whitespace
+        'macroname': [
+            (var_name, Name.Macro, '#pop'),
+        ],
+
+        'structname': [
+            (var_name, Name.Class, '#pop'),
+        ],
+
+        'funcname': [
+            (var_name, Name.Function, '#pop'),
+        ],
+
+        'builtins': [
+            (r'\b(' + '|'.join(builtin_funcs) + r')\b', Name.Builtin),
+            (r'\b(' + '|'.join(builtin_vars) + r')\b', Name.Builtin),
+        ],
+
+        'widgets': [
+            (r'\b(' + '|'.join(builtin_widgets) + r')(\s+)('+var_name+r')\b', 
+             bygroups(Keyword.Builtin, Text, Name.Variable)),
         ]
     }
--- test.py
+++ test.py
@@ -3,19 +3,37 @@ from pygments_lexers.cksp_lexer import CKSPLexer
 from pygments.formatters import TerminalFormatter
 
 code = """
+
+struct List
+    declare value: string
+    declare next: List
+
+    function __init__(self, value: int, next: List)
+        self.value := value
+        self.next := next
+    end function
+end struct
+
+define GLOBAL_VAR := 42
+
 on init
+    declare ui_slider sli_test(0,1000)
     declare x: int, y: int := 42
     declare str: string := "Hello, World!"
-    test(x)
+    test($x)
+    message(EVENT_NOTE, 3, 5, ALL_EVENTS[0])
 end on
 
 function test(x: int, y: int)
     if (x > 10 and y < 20)
         return true
     else
+        message("Hello, World")
         return false
     end if
 end function
+
+
 """
 
 lexer = CKSPLexer()
